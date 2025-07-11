0989ea376017e1c50c48d764e4173e1991878eb4
Raave Aires <dev@raave.me>
2025-07-11 18:50:22 -0300

    feat: adiciona action de envio de e-mail de confirmação

---
+++ lib/actions/send-verification-email.ts
@@ -0,0 +1,54 @@
+"use server";
+
+// dependências:
+import { Resend } from "resend";
+
+//componentes de e-mail:
+import { VerificationEmail } from "@/components/emails/verification-email";
+
+interface SendVerificationEmailProps {
+  name: string;
+  email: string;
+  verifyUrl: string;
+}
+
+interface SendVerificationEmailResult {
+  success: boolean;
+  message?: string;
+  data?: string;
+}
+
+const resend = new Resend(process.env.RESEND_API_KEY);
+
+export async function SendVerificationEmail(
+  _prevState: SendVerificationEmailResult | null,
+  {
+    name,
+    email,
+    verifyUrl,
+  }: SendVerificationEmailProps ): Promise<SendVerificationEmailResult> {
+  const { data, error } = await resend.emails.send({
+    from: "Zatjini <confirm@zatjini.org>",
+    to: [email],
+    subject: "Confirme seu e-mail · Zatjini",
+    react: VerificationEmail({
+      name: name,
+      email: email,
+      verifyUrl: verifyUrl as string,
+    }),
+    text: `Oi, ${name}! Antes de liberar seu acesso ao Zatjini, precisamos confirmar que este e-mail (${email}) é realmente seu. É rapidinho, só clicar aqui no link: ${verifyUrl}`
+  });
+
+  if (error) {
+    return {
+      success: false,
+      message: "Houve um erro na requisição do seu e-mail de confirmação. Por favor, tente novamente."
+    }
+  }
+
+  return {
+    success: true,
+    message: "Enviamos um e-mail de confirmação para você.",
+    data: data?.id
+  }
+}
