8c1bee9b12a22147a0dbe4a6840f26dfd986c62c
Raave Aires <dev@raave.me>
2025-07-15 19:34:04 -0300

    feat: adiciona os botões de autenticação oauth

--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -4,14 +4,18 @@ import { AuthHeader } from "@/components/auth/card-header";
 
 import { RegisterForm } from "@/components/auth/forms/register";
 import { AuthFooter } from "@/components/auth/card-footer";
+import { RenderOauthButtons } from "@/components/renderers/oath-buttons";
+import { Or } from "@/components/pieces/or";
 
 export default function Page() {
   return (
     <>
       <AuthHeader auth_type="register" />
 
-      <CardContent>
+      <CardContent className="flex flex-col gap-4">
         <RegisterForm />
+        <Or texto="ou registre-se com"/>
+        <RenderOauthButtons />
       </CardContent>
 
       <AuthFooter auth_type="register" />
