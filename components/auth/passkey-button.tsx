fe3166cb275f64d47716ec21a16205efc739e939
Raave Aires <dev@raave.me>
2025-07-16 20:06:12 -0300

    feat: adiciona feedbacks para o botão de passkey, traz o componente para o lado do cliente

--- components/auth/passkey-button.tsx
+++ components/auth/passkey-button.tsx
@@ -1,24 +1,63 @@
+"use client";
+
+// dependências:
+import { useState } from "react";
+import { redirect } from "next/navigation";
+
 // componentes:
 import { Button } from "@/components/ui/button";
 
 // ícones:
-import { Key } from "lucide-react";
+import { Check, Key, X } from "lucide-react";
+import { toast } from "sonner";
 
 // funções:
 import { authClient } from "@/lib/auth-client";
+import { Loader } from "../pieces/loader";
+import { getErrorMessage } from "@/lib/errors";
 
-export function PasskeyButton(){
-  async function SignInWithPasskey(){
-    await authClient.signIn.passkey({ autoFill: true })
-  };
+export function PasskeyButton() {
+  const [isPasskeyPending, setIsPasskeyPending] = useState(false);
+  const [passkeyShowCheck, setPasskeyShowCheck] = useState(false);
+  const [passkeyShowErrorFlash, setPasskeyShowErrorFlash] = useState(false);
 
-  return(
-    <Button
-      variant="secondary"
-      type="button"
-      onClick={SignInWithPasskey}
-    >
-      <Key /> Entrar com Passkey
+  async function SignInWithPasskey() {
+    await authClient.signIn.passkey(
+      { autoFill: true },
+      {
+        onRequest: () => {
+          setIsPasskeyPending(true);
+        },
+        onSuccess: (ctx) => {
+          setIsPasskeyPending(false);
+          setPasskeyShowCheck(true);
+          setTimeout(() => setPasskeyShowCheck(false), 2000);
+          toast.success(`Bem-vindo(a), ${ctx.data.user.name}!`);
+          redirect("/");
+        },
+        onError: (ctx) => {
+          setIsPasskeyPending(false);
+          setPasskeyShowErrorFlash(true);
+          setTimeout(() => setPasskeyShowErrorFlash(false), 2000);
+          toast.error(getErrorMessage(ctx.error.code));
+        },
+      }
+    );
+  }
+
+  return (
+    <Button variant="outline" type="button" onClick={SignInWithPasskey}>
+      {isPasskeyPending ? (
+        <Loader />
+      ) : passkeyShowCheck ? (
+        <Check size={18} />
+      ) : passkeyShowErrorFlash ? (
+        <X size={18} />
+      ) : (
+        <>
+          <Key /> Entrar com Passkey
+        </>
+      )}
     </Button>
   );
-};
+}
