99a1cd0d5152aca0150cc13339796c1aef712b70
Raave Aires <dev@raave.me>
2025-07-16 18:53:21 -0300

    wip: cria o botão de login com passkey

--- components/auth/passkey-button.tsx
+++ components/auth/passkey-button.tsx
@@ -1,15 +1,22 @@
+// componentes:
 import { Button } from "@/components/ui/button";
+
+// ícones:
 import { Key } from "lucide-react";
+
+// funções:
+import { authClient } from "@/lib/auth-client";
 
 export function PasskeyButton(){
   async function SignInWithPasskey(){
-    await 
-  }
+    await authClient.signIn.passkey({ autoFill: true })
+  };
+
   return(
     <Button
       variant="secondary"
       type="button"
-      onClick={()=>alert("Isto")}
+      onClick={SignInWithPasskey}
     >
       <Key /> Entrar com Passkey
     </Button>
