46219754f3b24f4177ab743dd2e7ef11299e69d5
Raave Aires <dev@raave.me>
2025-07-16 20:08:46 -0300

    refactor: remove as versões em inglês dos erros

--- lib/errors.ts
+++ lib/errors.ts
@@ -1,10 +1,9 @@
-import { authClient } from "./auth-client";
+import { authClient } from "@/lib/auth-client";
 
 type ErrorTypes = Partial<
 	Record<
 		keyof typeof authClient.$ERROR_CODES,
 		{
-			en: string;
 			pt_br: string;
 		}
 	>
@@ -12,23 +11,19 @@ type ErrorTypes = Partial<
  
 const errorCodes = {
 	USER_ALREADY_EXISTS: {
-		en: authClient.$ERROR_CODES.USER_ALREADY_EXISTS,
-		pt_br: "Usuário já existe."
+		pt_br: "Esse e-mail já está registrado."
 	},
   INVALID_EMAIL_OR_PASSWORD: {
-    en: authClient.$ERROR_CODES.INVALID_EMAIL_OR_PASSWORD,
     pt_br: "Login ou senha inválidos."
   },
 	EMAIL_NOT_VERIFIED: {
-		en: authClient.$ERROR_CODES.EMAIL_NOT_VERIFIED,
 		pt_br: "Verifique seu e-mail antes de continuar."
-	}
-  
+	},
 } satisfies ErrorTypes;
  
-export const getErrorMessage = (code: string, lang: "en" | "pt_br" = "pt_br") => {
+export const getErrorMessage = (code: string, lang: "pt_br" = "pt_br") => {
 	if (code in errorCodes) {
 		return errorCodes[code as keyof typeof errorCodes][lang];
 	}
-	return "";
+	return "Ocorreu um erro inesperado. Por favor, tente novamente.";
 };
